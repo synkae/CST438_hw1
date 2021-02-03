b302b2dc2f650367365658357640d63065b76e8c
synkae <synkae123@gmail.com>
2021-02-03 14:05:13 -0800

    Initial commit

---
+++ app/src/main/java/com/codepath/synkae/hw1/JsonPlaceHolderApi.java
@@ -0,0 +1,11 @@
+package com.codepath.synkae.hw1;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+    @GET("posts")
+    Call<List<Post>> getPosts();
+
+}
---
+++ app/src/main/java/com/codepath/synkae/hw1/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.codepath.synkae.hw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+    }
+
+}
